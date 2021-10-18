a0f9a7b29960fc40ca83edf5b9c3e80ff30b9025
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2021-10-18 19:34:03 +0000

    format change

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,7 +3,8 @@
 Take two floats as arguments and return their sum as a float
 '''
 
+
 def concat(str1: str, str2: str) -> str:
-  ''' Take two strings as arguments and return a concatenated string '''
-  return str1 + str2
+    ''' Take two strings as arguments and return a concatenated string '''
+    return str1 + str2
   
