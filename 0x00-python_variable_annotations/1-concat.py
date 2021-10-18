d7be4b0bcb596e3785f7371847194e09a149d415
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2021-10-18 19:12:34 +0000

    clean up

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+Take two floats as arguments and return their sum as a float
+'''
+
+def concat(str1: str, str2: str) -> str:
+  ''' Take two strings as arguments and return a concatenated string '''
+  return str1 + str2
+  
